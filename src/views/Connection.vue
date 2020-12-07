ca44aed2826c496ec80457fc4fb0a102577d656f
Herms Stanley <hstanley@mdcottawa.ca>
2020-12-07 11:49:24 -0500

    update120720 1148am

--- src/views/Connection.vue
+++ src/views/Connection.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>
-      <h1>Premium</h1>
-      <p class="border-top"></p>
-    </div>
+<div>
+  <b-container :fluid="true" class="accounting">
+    <b-row>
+      <b-col>
+        <div class="d-flex justify-content-between">
+          <h1>Premium</h1>
+          <div>
+            <b-button-group>
+              <b-button variant="outline-secondary">Share</b-button>
+              <b-button class="mr-2" variant="outline-secondary">Export</b-button>
+            </b-button-group>
+            <dropdown-bar-item v-for="(dd, key) in dropdownBar"
+                               v-bind="dd"
+                               :key="key" />
+                </div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+  <p class="border-top"></p>
+  </div>
 </template>
+<script>
+import DropdownBarItem from '@/components/DropdownBarItem.vue';
+import userDropdown from '@/util/userDropdown';
+
+export default {
+  components: { DropdownBarItem },
+  data: () => ({
+    dropdownBar: [{
+      name: '',
+      icon: 'CropIcon',
+      expanded: true,
+      items: [
+        { name: 'Test', icon: 'FileTextIcon' },
+        { name: 'Test1', icon: 'FileTextIcon' },
+        null,
+        { name: 'Test2', icon: 'FileTextIcon' },
+        null,
+        { name: 'Test3', icon: 'FileTextIcon' }
+      ]
+    }].concat(userDropdown)
+  })
+};
+</script>
---
+++ src/views/SalesHistory.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <h4>Storage & Connection</h4>
+    </div>
+</template>
