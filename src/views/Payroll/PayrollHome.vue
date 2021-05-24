eeb90f65146a7d18d638d93896c9c294e159c668
Herms Stanley <scatterinc@gmail.com>
2021-05-24 16:03:41 -0400

    bootstraptree view

--- src/views/Payroll/PayrollHome.vue
+++ src/views/Payroll/PayrollHome.vue
@@ -4,104 +4,63 @@
             <b-col>
                 <router-view><div></div></router-view>
             </b-col>
-            <b-col cols="3" class="bg-light border-left">
-                <div class="text-right">                   <b-form-checkbox v-model="checked"  name="PayrollEmployeecheck-button"  switch>
+            <b-col style="min-height:680px" cols="3" class="bg-light border-left">
+                <div class="text-right">
+                  <b-form-checkbox v-model="checked"  name="PayrollEmployeecheck-button"  switch>
             </b-form-checkbox></div>
                <div>
 
-  <b-nav v-b-toggle.collapse-1 ><h4 class="mb-2">Directory of Employee</h4></b-nav>
-  <!-----Payroll Frequency---->
-  <b-collapse id="collapse-1" class="mt-1">
-             <b-nav v-b-toggle.collapse-1-inner size="sm">
-      <b-form-checkbox
-      id="checkbox-1"
-      v-model="status"
-      name="checkbox-1"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >    </b-form-checkbox>
-                 Monthly</b-nav>
-             <!-----Position---->
-            <b-collapse id="collapse-1-inner" class="mt-1 ml-3">
-                <b-nav class="ml-6" v-b-toggle.collapse-2-inner size="sm">
-      <b-form-checkbox
-      id="checkbox-2"
-      v-model="status"
-      name="checkbox-2"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >    </b-form-checkbox>
-                    Position</b-nav>
-                         <!-----Employee---->
-                    <b-collapse id="collapse-2-inner" class="mt-1 ml-5">
-                                              <b-nav>
-      <b-form-checkbox
-      id="checkbox-7"
-      v-model="status"
-      name="checkbox-7"
-      value="accepted"
-      unchecked-value="not_accepted">
-      <feather-icon size="1x" icon="UserCheckIcon" />Employee Manager
-         </b-form-checkbox>
-         </b-nav>
-                    </b-collapse>
-            </b-collapse>
-            <!-----Payroll Frequency2---->
-            <b-nav v-b-toggle.collapse-3-inner size="sm">
-      <b-form-checkbox
-      id="checkbox-3"
-      v-model="status"
-      name="checkbox-3"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >    </b-form-checkbox>
-                Fortnightly</b-nav>
-             <!-----Position---->
-            <b-collapse id="collapse-3-inner" class="mt-1 ml-3">
-                <b-nav class="ml-6" v-b-toggle.collapse-4-inner size="sm">
-        <b-form-checkbox
-      id="checkbox-4"
-      v-model="status"
-      name="checkbox-4"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >    </b-form-checkbox>
-                    Position</b-nav>
-                         <!-----Employee---->
-                    <b-collapse id="collapse-4-inner" class="mt-1 ml-5">
-                        <b-nav>
-      <b-form-checkbox
-      id="checkbox-5"
-      v-model="status"
-      name="checkbox-5"
-      value="accepted"
-      unchecked-value="not_accepted">
-      <feather-icon size="1x" icon="UserCheckIcon" />Employee
-         </b-form-checkbox>
-         </b-nav>
-                                 <b-nav>
-      <b-form-checkbox
-      id="checkbox-6"
-      v-model="status"
-      name="checkbox-6"
-      value="accepted"
-      unchecked-value="not_accepted">
-      <feather-icon size="1x" icon="UserCheckIcon" />Employee2
-         </b-form-checkbox>
-         </b-nav>
-                    </b-collapse>
-            </b-collapse>
-  </b-collapse>
-</div>
+  <div><h4 class="mb-2">Directory of Employee</h4></div>
+  <b-tree-view  style="color:red" :data="treeData"></b-tree-view>
+      </div>
             </b-col>
         </b-row>
     </div>
 </template>
 <script>
+
 export default {
   name: 'PayrollHome',
   data: () => ({
-    checked: true
+    checked: true,
+    treeData: [{
+      id: 2,
+      name: 'All',
+      children: [{
+        id: 3,
+        name: 'Monthly',
+        children: [{
+          id: 4,
+          name: 'Manager',
+          children: [{
+            id: 5,
+            name: 'Snow Ariel'
+          },
+          {
+            id: 6,
+            name: 'Doe Jane'
+          }]
+        },
+        {
+          id: 7,
+          name: 'Accountant'
+        }]
+      },
+      {
+        id: 8,
+        name: 'Fortnight',
+        children: [{
+          id: 9,
+          name: 'Line Cook',
+          children: [{
+            id: 10,
+            name: 'Miguel Rose'
+          }]
+        }]
+      }]
+
+    }]
+
   })
 };
 </script>
